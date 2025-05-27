1243341aa2f8c93f6c4c8ad2da289097c516eb56
roni5604 <roniro5604@gmail.com>
2025-05-27 13:57:19 +0300

    new

--- src/app.py
+++ src/app.py
@@ -184,3 +184,5 @@ if __name__ == "__main__":
     threading.Thread(target=satellite_updater, daemon=True).start()
     print("[Tracker] Flask server starting on port 5003")
     app.run(host="0.0.0.0", port=5003)
+
+    print("[Tracker] Flask server started successfully.")
